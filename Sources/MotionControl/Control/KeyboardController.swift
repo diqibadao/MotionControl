558fbdddf357855f49f232608620cfe83514362f
Your Name <you@example.com>
2026-05-27 18:03:44 +0800

    refactor: 移除 KeyboardController 的 public 访问控制

--- Sources/MotionControl/Control/KeyboardController.swift
+++ Sources/MotionControl/Control/KeyboardController.swift
@@ -2,12 +2,12 @@ import Foundation
 import CoreGraphics
 
 /// 键盘控制器，使用 CGEvent 模拟按键。
-public class KeyboardController {
+class KeyboardController {
 
-    public init() {}
+    init() {}
 
     /// 按下并释放单个键
-    public func pressKey(_ keyCode: CGKeyCode) {
+    func pressKey(_ keyCode: CGKeyCode) {
         guard let down = CGEvent(keyboardEventSource: nil, virtualKey: keyCode, keyDown: true) else { return }
         guard let up = CGEvent(keyboardEventSource: nil, virtualKey: keyCode, keyDown: false) else { return }
         down.post(tap: .cghidEventTap)
@@ -15,7 +15,7 @@ public class KeyboardController {
     }
 
     /// 发送组合键（例如 Command+Q）
-    public func sendKeyCombo(_ keyCode: CGKeyCode, flags: CGEventFlags = []) {
+    func sendKeyCombo(_ keyCode: CGKeyCode, flags: CGEventFlags = []) {
         guard let downFlags = CGEvent(keyboardEventSource: nil, virtualKey: keyCode, keyDown: true) else { return }
         downFlags.flags = flags
         downFlags.post(tap: .cghidEventTap)
@@ -25,7 +25,7 @@ public class KeyboardController {
     }
 
     /// 执行系统命令（通过快捷键模拟）
-    public func executeSystemCommand(_ command: SystemCommand) {
+    func executeSystemCommand(_ command: SystemCommand) {
         switch command {
         case .missionControl:
             sendKeyCombo(0x7D, flags: .maskControl) // F3 可通过 keycode? 简化使用 Mission Control 是 control+up
@@ -51,11 +51,11 @@ public class KeyboardController {
     }
 
     // 常用键码
-    public static let kVK_Space: CGKeyCode = 0x31
-    public static let kVK_Return: CGKeyCode = 0x24
-    public static let kVK_Delete: CGKeyCode = 0x33
-    public static let kVK_UpArrow: CGKeyCode = 0x7E
-    public static let kVK_DownArrow: CGKeyCode = 0x7D
-    public static let kVK_LeftArrow: CGKeyCode = 0x7B
-    public static let kVK_RightArrow: CGKeyCode = 0x7C
+    static let kVK_Space: CGKeyCode = 0x31
+    static let kVK_Return: CGKeyCode = 0x24
+    static let kVK_Delete: CGKeyCode = 0x33
+    static let kVK_UpArrow: CGKeyCode = 0x7E
+    static let kVK_DownArrow: CGKeyCode = 0x7D
+    static let kVK_LeftArrow: CGKeyCode = 0x7B
+    static let kVK_RightArrow: CGKeyCode = 0x7C
 }
